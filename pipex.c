4170d8e0e7b22a44fd873c4f926a8a0cd88829db
maxmaison <mdsiurds@gmail.com>
2025-03-10 21:44:50 +0100

    essais

--- pipex.c
+++ pipex.c
@@ -6,9 +6,15 @@
 /*   By: maxoph <maxoph@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/07 16:14:33 by maxoph            #+#    #+#             */
-/*   Updated: 2025/03/07 18:15:39 by maxoph           ###   ########.fr       */
+/*   Updated: 2025/03/10 17:26:02 by maxoph           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <unistd.h> // pour fork, pipe, execve, etc.
+#include <stdlib.h> // pour malloc, free, exit
+#include <stdio.h> // pour perror
+#include <fcntl.h> // pour open, O_RDONLY, etc.
+#include <sys/wait.h> // pour waitpid
 
 int	main(int argc, char **argv)
 {
@@ -28,7 +34,7 @@ typedef struct s_pipex
 	char	**cmd2_args;
 	char	*cmd1_path;
 	char	*cmd2_path;
-	int		pipe_fd[2];
+	int		pipe_fd[2];// Descripteurs du pipe [0]=lecture, [1]=écriture
 	int		pid1;
 	int		pid2;
 	int		infile_fd;
@@ -78,4 +84,18 @@ void debug_print_pipex(t_pipex *pipex)
     printf("pipe_fd: [%d, %d]\n", pipex->pipe_fd[0], pipex->pipe_fd[1]);
     printf("pid1: %d, pid2: %d\n", pipex->pid1, pipex->pid2);
     printf("-------------------------\n");
+}
+
+exit(127);
+0 : succès
+1 : erreur générale
+126 : commande trouvée mais non exécutable
+127 ; command not found
+130 : programme interrompu par CTRL+C
+
+pipex.infile_fd = open(pipex.infile, O_RDONLY);
+if (pipex.infile_fd == -1) // Si le fichier dentree nexiste pas 
+{
+    perror(pipex.infile);  // Affiche l'erreur
+    pipex.infile_fd = open("/dev/null", O_RDONLY);  // Utilise le trou noir
 }
